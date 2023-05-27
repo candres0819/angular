a9e58d66245463d4b9c822a4c91d493d70788215
Carlos Cardona <candres0819@gmail.com>
2023-05-26 23:56:57 -0500

    fix

--- trello-clone/src/app/app-routing.module.ts
+++ trello-clone/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { BoardsComponent } from './pages/boards/boards.component';
 
 const routes: Routes = [{
   path: 'login',
-  component: LoginComponent 
+  component: LoginComponent
+}, {
+  path: 'boards',
+  component: BoardsComponent
 }];
 
 @NgModule({
